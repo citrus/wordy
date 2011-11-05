07f7cb8a1aacddaac1914ee4da9dfd4fb6179b7a
Spencer Steffen <spencer@citrusme.com>
2011-11-05 10:38:45 -0700

    add some more tests

--- test/test_wordy.rb
+++ test/test_wordy.rb
@@ -11,20 +11,40 @@ class TestWordy < MiniTest::Unit::TestCase
     assert_equal Array, Wordy::WORDS.class
   end
 
-  def test_single_word
+  def test_single_word_has_no_spaces
     refute_match /\s/, Wordy.word
   end
-
+  
+  def test_single_word_is_string
+    assert_equal String, Wordy.word.class
+  end
+  
+  def test_single_word_is_not_blank
+    assert 1 < Wordy.word.length
+  end
+  
   def test_words
     assert_equal Array, Wordy.words.class
   end
+  
+  def test_words_contain_strings
+    assert_equal String, Wordy.words.first.class
+  end
 
-  def test_sentance
+  def test_sentence
     assert_match /^[A-Z].*\.$/, Wordy.sentence
+  end
+  
+  def test_sentence_has_length
+    assert 5 < Wordy.sentence.length
   end
 
   def test_sentences
     assert_equal Array, Wordy.sentences.class
+  end
+
+  def test_sentences_contain_strings
+    assert_equal String, Wordy.sentences.first.class
   end
   
   def test_paragraph
@@ -33,6 +53,10 @@ class TestWordy < MiniTest::Unit::TestCase
   
   def test_paragraphs
     assert_equal Array, Wordy.paragraphs.class
+  end
+  
+  def test_paragraphs_contain_strings
+    assert_equal String, Wordy.paragraphs.first.class
   end
   
   def test_body
